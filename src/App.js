2c9b63f8404d47350386fb5018bce396a1455215
Kōhei Yamamoto <kymmt90@gmail.com>
2017-04-22 18:36:44 +0900

    Get credentials from the log-in form (#4)
    * Submit information for logging in and get JWT
    
    * Send the authorization header if it exists

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import LoginForm from './LoginForm';
 import Paginator from './Paginator';
 import parse from 'parse-link-header';
 import React, { Component } from 'react';
@@ -17,7 +18,8 @@ class App extends Component {
       firstPage: {},
       lastPage: {},
       nextPage: {},
-      previousPage: {}
+      previousPage: {},
+      jwt: ''
     }
 
     this.fetchSnippets = this.fetchSnippets.bind(this);
@@ -27,7 +29,12 @@ class App extends Component {
     this.handleClickSnippetTitle = this.handleClickSnippetTitle.bind(this);
     this.handleClickBackToIndex = this.handleClickBackToIndex.bind(this);
     this.handleClickUserName = this.handleClickUserName.bind(this);
+    this.handleClickLogIn = this.handleClickLogIn.bind(this);
+    this.moveToLogInPage = this.moveToLogInPage.bind(this);
     this.getCurrentPage = this.getCurrentPage.bind(this);
+    this.getUserToken = this.getUserToken.bind(this);
+    this.getRequestWithAuth = this.getRequestWithAuth.bind(this);
+    this.postRequestWithAuth = this.postRequestWithAuth.bind(this);
 
     this.fetchSnippets('http://localhost:3001/snippets');
   }
@@ -52,6 +59,17 @@ class App extends Component {
     this.fetchUserSnippets(event.target.href);
   }
 
+  handleClickLogIn(event) {
+    event.preventDefault();
+    this.moveToLogInPage();
+  }
+
+  moveToLogInPage() {
+    this.setState({
+      currentLocation: 'log_in'
+    });
+  }
+
   getCurrentPage() {
     if (!_.isEmpty(this.state.nextPage)) {
       return this.toDecimalNumber(this.state.nextPage.number) - 1;
@@ -60,13 +78,40 @@ class App extends Component {
     }
   }
 
+  getUserToken(email, password) {
+    const params = {
+      auth: {
+        email: email,
+        password: password
+      }
+    };
+
+    this.postRequestWithAuth('http://localhost:3001/user_token')
+      .send(params)
+      .end((err, res) => {
+        if (err) {
+          console.log(err);
+        } else {
+          if (res.body.jwt) {
+            this.setState({
+              jwt: res.body.jwt,
+              currentLocation: 'snippets'
+            });
+          } else {
+            this.setState({
+              jwt: ''
+            });
+          }
+        }
+      });
+  }
+
   toDecimalNumber(intRepresentation) {
     return parseInt(intRepresentation, 10)
   }
 
   fetchSnippet(url) {
-    request
-      .get(url)
+    this.getRequestWithAuth(url)
       .end((err, res) => {
         if (err) {
           console.log(err);
@@ -80,8 +125,7 @@ class App extends Component {
   }
 
   fetchSnippets(url) {
-    request
-      .get(url)
+    this.getRequestWithAuth(url)
       .end((err, res) => {
         if (err) {
           console.log(err);
@@ -96,8 +140,7 @@ class App extends Component {
   }
 
   fetchUserSnippets(url) {
-    request
-      .get(url)
+    this.getRequestWithAuth(url)
       .end((err, res) => {
         if (err) {
           console.log(err);
@@ -109,6 +152,22 @@ class App extends Component {
           this.setPagesToState(res);
         }
       });
+  }
+
+  getRequestWithAuth(url) {
+    if (this.state.jwt) {
+      return request.get(url).accept('json').set('Authorization', `Bearer ${this.state.jwt}`);
+    } else {
+      return request.get(url).accept('json');
+    }
+  }
+
+  postRequestWithAuth(url) {
+    if (this.state.jwt) {
+      return request.post(url).accept('json').set('Authorization', `Bearer ${this.state.jwt}`);
+    } else {
+      return request.post(url).accept('json');
+    }
   }
 
   setPagesToState(res) {
@@ -167,9 +226,16 @@ class App extends Component {
           <a href="http://localhost:3001/snippets" onClick={this.handleClickPaginator}>Back to all snippets</a>
         </div>
       );
+    } else if (this.state.currentLocation === 'log_in') {
+      return (
+        <div className="App">
+          <LoginForm getUserToken={this.getUserToken} />
+        </div>
+      );
     } else {
       return (
         <div className="App">
+          <a href="#" onClick={this.handleClickLogIn}>Log in</a>
           <SnippetsList snippets={this.state.snippets} onClickTitle={this.handleClickSnippetTitle} onClickUserName={this.handleClickUserName} />
           <Paginator first={this.state.firstPage} previous={this.state.previousPage} next={this.state.nextPage} last={this.state.lastPage} onClick={this.handleClickPaginator} />
         </div>
