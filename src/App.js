4d711fecfc0c72a726d55483e01ff7b078ebc28c
Kōhei Yamamoto <kymmt90@gmail.com>
2017-04-22 16:45:29 +0900

    User's snippets page (#3)
    * Enable to click each author name which links to the user's snippets pages
    
    * Add a returning link to all snippets
    
    * Fall back page location except specified ones to all snippet pages

--- src/App.js
+++ src/App.js
@@ -22,9 +22,11 @@ class App extends Component {
 
     this.fetchSnippets = this.fetchSnippets.bind(this);
     this.fetchSnippet = this.fetchSnippet.bind(this);
+    this.setPagesToState = this.setPagesToState.bind(this);
     this.handleClickPaginator = this.handleClickPaginator.bind(this);
     this.handleClickSnippetTitle = this.handleClickSnippetTitle.bind(this);
     this.handleClickBackToIndex = this.handleClickBackToIndex.bind(this);
+    this.handleClickUserName = this.handleClickUserName.bind(this);
     this.getCurrentPage = this.getCurrentPage.bind(this);
 
     this.fetchSnippets('http://localhost:3001/snippets');
@@ -43,6 +45,11 @@ class App extends Component {
   handleClickBackToIndex(event) {
     event.preventDefault();
     this.fetchSnippets(event.target.href);
+  }
+
+  handleClickUserName(event) {
+    event.preventDefault();
+    this.fetchUserSnippets(event.target.href);
   }
 
   getCurrentPage() {
@@ -80,64 +87,91 @@ class App extends Component {
           console.log(err);
         } else {
           this.setState({
-            currentLocation: 'snippets'
+            currentLocation: 'snippets',
+            snippets: res.body
           });
-
-          const linkHeader = parse(res.headers['link']);
-
-          this.setState({
-            snippets: res.body,
-            firstPage: {
-              number: linkHeader.first.page,
-              url: linkHeader.first.url
-            },
-            lastPage: {
-              number: linkHeader.last.page,
-              url: linkHeader.last.url
-            }
-          });
-
-          if (linkHeader.previous) {
-            this.setState({
-              previousPage: {
-                number: linkHeader.previous.page,
-                url: linkHeader.previous.url
-              }
-            });
-          } else {
-            this.setState({
-              previousPage: {}
-            });
-          }
-
-          if (linkHeader.next) {
-            this.setState({
-              nextPage: {
-                number: linkHeader.next.page,
-                url: linkHeader.next.url
-              }
-            });
-          } else {
-            this.setState({
-              nextPage: {}
-            });
-          }
+          this.setPagesToState(res);
         }
       });
   }
 
+  fetchUserSnippets(url) {
+    request
+      .get(url)
+      .end((err, res) => {
+        if (err) {
+          console.log(err);
+        } else {
+          this.setState({
+            currentLocation: 'user',
+            snippets: res.body
+          });
+          this.setPagesToState(res);
+        }
+      });
+  }
+
+  setPagesToState(res) {
+    const linkHeader = parse(res.headers['link']);
+
+    this.setState({
+      firstPage: {
+        number: linkHeader.first.page,
+        url: linkHeader.first.url
+      },
+      lastPage: {
+        number: linkHeader.last.page,
+        url: linkHeader.last.url
+      }
+    });
+
+    if (linkHeader.previous) {
+      this.setState({
+        previousPage: {
+          number: linkHeader.previous.page,
+          url: linkHeader.previous.url
+        }
+      });
+    } else {
+      this.setState({
+        previousPage: {}
+      });
+    }
+
+    if (linkHeader.next) {
+      this.setState({
+        nextPage: {
+          number: linkHeader.next.page,
+          url: linkHeader.next.url
+        }
+      });
+    } else {
+      this.setState({
+        nextPage: {}
+      });
+    }
+  }
+
   render() {
-    if (this.state.currentLocation === 'snippets') {
-      return (
-        <div className="App">
-          <SnippetsList snippets={this.state.snippets} onClickTitle={this.handleClickSnippetTitle} />
-          <Paginator first={this.state.firstPage} previous={this.state.previousPage} next={this.state.nextPage} last={this.state.lastPage} onClick={this.handleClickPaginator} />
-        </div>
-      );
-    } else if (this.state.currentLocation === 'snippet') {
+    if (this.state.currentLocation === 'snippet') {
       return (
         <div className="App">
           <Snippet title={this.state.snippet.title} author={this.state.snippet.author} content={this.state.snippet.content} currentPage={this.getCurrentPage()} onClickBackToIndex={this.handleClickBackToIndex} />
+        </div>
+      );
+    } else if (this.state.currentLocation === 'user') {
+      return (
+        <div className="App">
+          <SnippetsList snippets={this.state.snippets} onClickTitle={this.handleClickSnippetTitle} onClickUserName={this.handleClickUserName} />
+          <Paginator first={this.state.firstPage} previous={this.state.previousPage} next={this.state.nextPage} last={this.state.lastPage} onClick={this.handleClickPaginator} />
+          <a href="http://localhost:3001/snippets" onClick={this.handleClickPaginator}>Back to all snippets</a>
+        </div>
+      );
+    } else {
+      return (
+        <div className="App">
+          <SnippetsList snippets={this.state.snippets} onClickTitle={this.handleClickSnippetTitle} onClickUserName={this.handleClickUserName} />
+          <Paginator first={this.state.firstPage} previous={this.state.previousPage} next={this.state.nextPage} last={this.state.lastPage} onClick={this.handleClickPaginator} />
         </div>
       );
     }
--- src/MetaData.js
+++ src/MetaData.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class MetaData extends Component {
   render() {
     return (
-      <h1><a href={`http://localhost:3001/snippets/${this.props.snippetId}`} onClick={this.props.onClickTitle}>{this.props.title}</a> / {this.props.author}</h1>
+        <h1><a href={`http://localhost:3001/snippets/${this.props.snippetId}`} onClick={this.props.onClickTitle}>{this.props.title}</a> / <a href={`http://localhost:3001/users/${this.props.author.id}/snippets`} onClick={this.props.onClickUserName}>{this.props.author.name}</a></h1>
     );
   }
 }
