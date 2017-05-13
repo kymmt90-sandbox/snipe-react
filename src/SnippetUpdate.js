8fc137750424e61a572bcf40716a892e2aa232ae
Kohei Yamamoto <kymmt90@gmail.com>
2017-05-13 22:25:11 +0900

    Add editing snippets page

---
+++ src/SnippetUpdate.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import SnippetEditor from './SnippetEditor.js';
+
+class SnippetUpdate extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Update the snippet</h1>
+        <SnippetEditor id={this.props.id} getRequestWithAuth={this.props.getRequestWithAuth} patchRequestWithAuth={this.props.patchRequestWithAuth} />
+      </div>
+    );
+  }
+}
+
+export default SnippetUpdate;
