0d12aa216c0e6798854e4062987c8b8b55a5c5f7
Kōhei Yamamoto <kymmt90@gmail.com>
2017-05-14 12:15:15 +0900

    Add snippet delete button (#8)
    * Add SnippetEditor#isEdit
    
    * Display the delete button only if edit mode
    
    * Extract generating the delete button as a function
    
    * Enable to delete snippets by authenticated user
    
    * No need to make `completed` true when deleting snippets

--- src/SnippetEditor.js
+++ src/SnippetEditor.js
@@ -11,17 +11,22 @@ class SnippetEditor extends Component {
       content: '',
       resultSnippetId: null,
       completed: false,
+      deleted: false,
     };
 
     this.handleTitleChange = this.handleTitleChange.bind(this);
     this.handleContentChange = this.handleContentChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleDeleteSubmit = this.handleDeleteSubmit.bind(this);
     this.postSnippet = this.postSnippet.bind(this);
+    this.deleteSnippet = this.deleteSnippet.bind(this);
     this.submitButtonText = this.submitButtonText.bind(this);
+    this.isEdit = this.isEdit.bind(this);
+    this.deleteButton = this.deleteButton.bind(this);
   }
 
   componentDidMount() {
-    if (!this.props.id) return;
+    if (!this.isEdit()) return;
 
     const snippetUrl = `http://localhost:3001/snippets/${this.props.id}`;
     this.props.getRequestWithAuth(snippetUrl)
@@ -52,12 +57,13 @@ class SnippetEditor extends Component {
   handleSubmit(event) {
     event.preventDefault();
     if (!_.isEmpty(this.state.title) && !_.isEmpty(this.state.content)) {
-      if (this.props.id) {
-        this.patchSnippet();
-      } else {
-        this.postSnippet();
-      }
+      this.isEdit() ? this.patchSnippet() : this.postSnippet();
     }
+  }
+
+  handleDeleteSubmit(event) {
+    event.preventDefault();
+    this.deleteSnippet();
   }
 
   postSnippet() {
@@ -100,29 +106,66 @@ class SnippetEditor extends Component {
       });
   }
 
+  deleteSnippet() {
+    const url = `http://localhost:3001/snippets/${this.props.id}`;
+    this.props.deleteRequestWithAuth(url)
+      .end((err, res) => {
+        if (err) {
+          console.log(err);
+        } else {
+          this.setState({
+            deleted: true,
+          });
+        }
+      });
+  }
+
   submitButtonText() {
-    return this.props.id ? 'Update snippet' : 'Create snippet'
+    return this.isEdit() ? 'Update snippet' : 'Create snippet'
+  }
+
+  isEdit() {
+    return !_.isEmpty(this.props.id);
+  }
+
+  deleteButton() {
+    if (this.isEdit()) {
+      return (
+        <form onSubmit={this.handleDeleteSubmit}>
+          <input type="submit" value="Delete" />
+        </form>
+      );
+    } else {
+      return null;
+    }
   }
 
   render() {
-    if (this.state.completed) {
+    if (this.state.deleted) {
+      return (
+        <Redirect to="/" />
+      );
+    } else if (this.state.completed) {
       const snippetUrl = `/snippets/${this.state.resultSnippetId}`
       return (
         <Redirect to={snippetUrl} />
       );
     } else {
       return (
-        <form onSubmit={this.handleSubmit}>
-          <label>
-            Title:
-            <input type="text" value={this.state.title} onChange={this.handleTitleChange} />
-          </label>
-          <label>
-            Content:
-            <textarea value={this.state.content} onChange={this.handleContentChange} />
-          </label>
-          <input type="submit" value={this.submitButtonText()} />
-        </form>
+        <div>
+          <form onSubmit={this.handleSubmit}>
+            <label>
+              Title:
+              <input type="text" value={this.state.title} onChange={this.handleTitleChange} />
+            </label>
+            <label>
+              Content:
+              <textarea value={this.state.content} onChange={this.handleContentChange} />
+            </label>
+            <input type="submit" value={this.submitButtonText()} />
+          </form>
+          {this.deleteButton()}
+        </div>
       );
     }
   }
--- src/SnippetUpdate.js
+++ src/SnippetUpdate.js
@@ -6,7 +6,7 @@ class SnippetUpdate extends Component {
     return (
       <div className="App">
         <h1>Update the snippet</h1>
-        <SnippetEditor id={this.props.id} getRequestWithAuth={this.props.getRequestWithAuth} patchRequestWithAuth={this.props.patchRequestWithAuth} />
+        <SnippetEditor id={this.props.id} getRequestWithAuth={this.props.getRequestWithAuth} patchRequestWithAuth={this.props.patchRequestWithAuth} deleteRequestWithAuth={this.props.deleteRequestWithAuth} />
       </div>
     );
   }
